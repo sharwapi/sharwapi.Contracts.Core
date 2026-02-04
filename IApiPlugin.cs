34604df59b7f62c6dd92196930f4a4f7d2e32d0f
sharworange <kuliangcha@qq.com>
2026-02-04 16:23:14 +0800

    添加代码注释

--- IApiPlugin.cs
+++ IApiPlugin.cs
@@ -6,28 +6,57 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace sharwapi.Contracts.Core
 {
+    /// <summary>
+    /// Defines the core interface for SharwAPI plugins.
+    /// </summary>
     public interface IApiPlugin
     {
-        //定义插件名称
+        /// <summary>
+        /// Gets the unique identifier name of the plugin.
+        /// </summary>
         string Name { get; }
-        //定义插件版本
+        /// <summary>
+        /// Gets the version of the plugin.
+        /// </summary>
         string Version { get; }
-        //定义插件显示名称
+        /// <summary>
+        /// Gets the display name of the plugin.
+        /// </summary>
         string DisplayName { get; }
-        //定义自动路由前缀的开关
+        /// <summary>
+        /// Gets a value indicating whether to enable automatic route prefixing.
+        /// <para>Defaults to <c>false</c>, but enabling it is recommended.</para>
+        /// </summary>
         bool UseAutoRoutePrefix 
         { 
             get => false;
         }
-        //定义插件注册服务方法
+        /// <summary>
+        /// Registers the plugin's services into the dependency injection container.
+        /// </summary>
+        /// <param name="services">The service collection to add services to.</param>
+        /// <param name="configuration">The application configuration properties.</param>
         void RegisterServices(IServiceCollection services, IConfiguration configuration);
-        //定义插件中间件配置方法
+        /// <summary>
+        /// Configures the HTTP request pipeline (middleware) for the plugin.
+        /// </summary>
+        /// <param name="app">The WebApplication instance.</param>
         void Configure(WebApplication app);
-        //定义插件路由注册方法
+        /// <summary>
+        /// Registers the plugin's business logic routes.
+        /// </summary>
+        /// <param name="app">The endpoint route builder.</param>
+        /// <param name="configuration">The application configuration properties.</param>
         void RegisterRoutes(IEndpointRouteBuilder app, IConfiguration configuration);
-        //定义插件提供的默认配置对象
+        /// <summary>
+        /// Gets the default configuration object provided by the plugin.
+        /// </summary>
         object? DefaultConfig => null;
-        //定义插件用于被管理的端点注册方法
+        /// <summary>
+        /// Registers endpoints used for plugin management.
+        /// <para><b>Note:</b> This is in the experimental prototyping stage and is not recommended for use.</para>
+        /// </summary>
+        /// <param name="managementGroup">The route group dedicated to management endpoints.</param>
         void RegisterManagementEndpoints(IEndpointRouteBuilder managementGroup)
         {
             managementGroup.MapGet("/", () =>
